c5fd1c34db6ae3ef35565f1d44fa0d360ed0a466
Justalike <justalike.ds@gmail.com>
2023-09-12 17:38:58 +0300

    filter almost done

--- telegram-options.js
+++ telegram-options.js
@@ -1,20 +1,15 @@
 let telegram = window.Telegram.WebApp;
-      let logo = document.getElementById('logo');
       telegram.expand()
       let submitButton = document.getElementById('sendButton');
 
       submitButton.addEventListener("click", () => {
-        let pair = document.getElementById('pair');
+        let pair = document.getElementById('pair')||document.getElementById('pairSuggestions');
         let timeframe = document.getElementById('timeframe');
         let longMA = document.getElementById('longMA-value');
         let shortMA = document.getElementById('shortMA-value');
         let longX = document.getElementById('longX-value');
         let shortX = document.getElementById('shortX-value');
-        console.log(longMA.value)
-        if (isNaN(longMA.value)) {
-          showError(longMA, 'Input must be a number');
-          return; // Stop the function execution if the validation fails
-        }
+       
         //  
         let data = {
           pair: pair.value,
@@ -25,6 +20,7 @@ let telegram = window.Telegram.WebApp;
           shortX: shortX.value,
 
         }
+        console.log(data)
 
         telegram.sendData(JSON.stringify(data))
 
