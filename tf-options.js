bfbeff82790a7d549c40eaa7b3296c03fc45db62
Justalike <justalike.ds@gmail.com>
2023-09-13 15:14:44 +0300

    updated timeframes

--- tf-options.js
+++ tf-options.js
@@ -2,7 +2,8 @@ const selectElement = document.getElementById("timeframe");
     
       // Define the available timeframes
       const timeframes = [
-        "1s", "5s", "10s", "30s", "1m", "5m", "15m", "30m", "1h", "4h", "1d", "1w", "1M"
+        "1s", "1m", "3m", '5m', "15m", "30m", "1h", "2h", "4h",
+         '6h', '8h', '12h', "1d", '3d', "1w", "1M"
       ];
     
       // Generate the options dynamically
